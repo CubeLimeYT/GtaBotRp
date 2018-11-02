eecdad5f70725fb1d10d45b9a78603f147a2b079
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2018-11-02 02:00:37 -1200

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,20 @@ const client = new Discord.Client();
 client.on('message', message => {
 
 
-	if(message.content === '//help') {
+	if(message.content === prefix + 'help') {
 		message.reply("Le bot est encore en dévellopement");
 	}
+	
+	if(message.content === prefix + 'inv'){
+		var embed = new Discord.RichEmbed()
+			.setTitle("Lien d'invitation")
+			.setDescription("Cliquez sur mon lien et emmenez moi avec vous")
+			.addField('https://discordapp.com/oauth2/authorize?client_id=507609010116100097&scope=bot&permissions=2146958847', 'Ce bot fonctionne avec une base de donnée, Certaine commande ne pourront ce faire seulement une fois')
+			.setColor("0x000000")
+			.setFooter("Support Server :https://discord.gg/8yXY3nd")
+		message.channel.sendEmbed(embed);
+	}
+
 
 });
 
