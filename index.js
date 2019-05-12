f563f4bb08d28cadb27d29a6099f6b29a115544e
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-05-12 18:00:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -650,4 +650,23 @@ bot.on('message', message => {
     }
 });
 
+client.on ("message", (message) => {
+	
+	msg = message.content.toLowerCase();
+	
+	if (message.author.bot) return;
+	
+	mention = message.mentions.users.first();
+	
+	if (msg.startWith (prefix + "send" )) {
+		if (mention == null) { return; }
+		message.delete();
+		mentionMessage = message.content.slice (8);
+		mentions.sendMessage (mentionMessage);
+		message.channel.send ("Envoyé !");
+	}
+
+	
+}
+
 bot.login(process.env.SECRET);
