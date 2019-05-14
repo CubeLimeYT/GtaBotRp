71e5ce341c6cc2d896cbc16c403e7782d3e96d9f
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-05-14 23:18:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,45 +24,6 @@ bot.on('ready', () => {
     console.log("Bot lancer !");
 });
 
-/////////////////////////////////Commandes Restart Owner Bot/////////////////
-
-bot.on('message',async message => {
-    if(message.content.startsWith(prefix + "restart")) {
-        if(message.author.id !== "372099632173416449") return message.reply('Vous n\'êtes pas le propriétaire du bot');
-        message.channel.send('**Redémarrage**').then(msg => {
-            setTimeout(() => {
-               msg.edit('**Redémarrage..**');
-            },1000);
-            setTimeout(() => {
-               msg.edit('**Redémarrage...**');
-            },2000);
-        console.log(`${message.author.tag} [ ${message.author.id} ] has restarted the bot.`);
-        console.log(`Restarting..`);
-        setTimeout(() => {
-            bot.destroy();
-            bot.login(process.env.SECRET);
-        },3000);
-          setTimeout(() => {
-               msg.edit('Bot redémarré ✅');
-            },2000);
-        });
-       
-	
-	//////////////////////////////Commandes Rp//////////////////////////
-
-
-    	if (message.content === prefix + 'rp') {
-    	message.delete().catch();
-        	if(message.guild.roles.find(r => r.name === "👌 Certifié 👌")){
-        		let role = message.guild.roles.find(r => r.name === "👌 Certifié 👌")
-        		let msg = await message.channel.send(`${role} qui pour Rp ?`);
-        		msg.react('✅');
-       			msg.react('❎');
-		}else{
-    			console.log("I don't find role");
-	}
-}
-}})
 
 
 ////////////////////////////////Commandes DM'S/////////////////////////
@@ -79,37 +40,7 @@ bot.on('message', message => {
         mention.sendMessage(mentionMessage);
         message.reply("message envoyé");
     }
-	
-     
-	//////////////////////////////////Commandes help///////////////////////////////    
-	if (message.content === prefix + 'aide' ) {
-		var helpEmbed = new Discord.RichEmbed()
-		.setDescription('Voici les commandes disponible pour le moment ')
-		.setColor("RANDOM")
-		.addField(`${prefix}dmap`, `Pour afficher les département de la map`)
-		.addField(`${prefix}bmap`, `Pour afficher les arrêts de bus de la map`)
-		.addField(`${prefix}rp`, `Pour afficher une sondage pour savoir qui veut Rp`)
-		.setFooter(`EN cours de mise à jour !`)
-		message.channel.send(helpEmbed)
-	}
 
-	//////////////////////////////////Commandes Map////////////////////////////
-	if (message.content === prefix + 'dmap') {
-        message.channel.send ( {files: ["./images/" + 1 + ".jpg"]} ) 
-    	}
-
-	if (message.content === prefix + 'bmap') {
-	message.channel.send ( {files: ["./images/" + 2 + ".png"]} )
-    	}
-	
-	///////////////////////////////Commandes Administration//////////////////////
-	
-	if (message.content === prefix + 'kick') {
-		const member = message.mentions.members.first();
-		const reason = args.slice(1).join("");
-		member.kick(reason);
-	}
-     
 })
 
 ///////////////////////////////FINISH///////////////////////////
