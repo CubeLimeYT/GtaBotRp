e2b887c0c7f9132bf2e9ebf69c68f0730c63b2de
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-05-20 00:23:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -138,50 +138,7 @@ if (message.content === prefix + 'help' ) {
     
     ///////////////////////////////Commandes Administration//////////////////////
     
-if ( !message.member.hasPermission('KICK_MEMBERS')){
-		return message.channel.send(`Vous n'avez pas les permission de kick`);
-	}
-    
-	let memberToKick = message.mentions.members.first();
-	if(memberToKick && memberToKick.kickable && (message.member.highestRole.calculatedPosition >
-            memberToKick.highestRole.calculatedPosition || message.guild.ownerID == message.author.id)){
-		let reason = tool.parseOptionArg('raison', message.content);
 
-	    let kickOptions = {
-	    	reason: reason ? reason: 'none'
-	    };
-	    memberToKick.kick(kickOptions);
-	    message.channel.send(` L\'utilisateur ${memberToKick} à bien été kick`);
-		let owner =  message.guild.owner;
-		try{
-			await owner.send({embed: {
-				title: "Kick",
-				color: 0x00FF,
-				fields:[{
-					name: "Utilisateur kick",
-					value: `${memberToKick}`,
-					inline: true
-				},
-				{
-					name: "Raison",
-					value: `${reason}`,
-					inline: true
-				},
-				{
-					name: "La personne qui a utliser la commande",
-					value: `${message.author}`,
-					inline: true
-				}]
-			}})
-		}catch(e){
-				message.reply(`Impossible d'envoyer le rapport a l'owner de la guild`);
-			 }
-
-	}else{
-        message.channel.send(`L\'utilisateur ${memberToKick} ne peut être kick`)
-    }
-
-}
      
 });
 
